827e5ffa129855b0b17958d1ac7601a3b0861ad8
Greg Copeland <gregory.copeland@me.com>
2018-10-11 11:24:30 -0400

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float moveSpeed = 500.0f;
+
+	private float movement = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxisRaw("Horizontal");
+	}
+
+	void FixedUpdate() {
+		rb.transform.RotateAround(Vector3.zero, Vector3.forward, -movement * Time.deltaTime * moveSpeed );
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex);
+	}
+}
